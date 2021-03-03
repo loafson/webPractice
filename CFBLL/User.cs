c5ddcbbf1b9644f5dbebbe28ceb420abcb6078b3
Chenfei <chnfei@outlook.com>
2021-03-03 18:03:41 +0800

    首次提交，添加数据访问辅助类，完成三层结构的搭建

---
+++ CFBLL/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CF.BLL
+{
+	public class User
+	{
+		public static bool add(Model.User user)
+		{
+			return DAL.User.add(user);
+		}
+	}
+}
---
+++ CFDAL/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CF.DAL
+{
+	/// <summary>
+	/// 用户数据访问类
+	/// </summary>
+	public class User
+	{
+		/// <summary>
+		/// 添加用户方法
+		/// </summary>
+		/// <param name="user"></param>
+		/// <returns></returns>
+		public static bool add(CF.Model.User user)
+		{
+			bool result = false;
+
+
+
+
+			return result;
+		}
+	}
+}
